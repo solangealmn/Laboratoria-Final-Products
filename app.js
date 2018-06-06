d01e85131b4a68a2fd02ef5b946ca4087ba19789
Solange Nalão <solangealmn@gmail.com>
2018-06-06 17:59:48 -0300

    Organização de cada passo do meu código.

--- app.js
+++ app.js
@@ -2,23 +2,25 @@
   function cipher(){
     //Obter o valor dos campos;
     var textoNormal = document.getElementById('cipher-entrance').value.toUpperCase();
+     console.log(textoNormal);
     //Obter a posição de cada letra;
     var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     function getNumberPosition(textoNormal){
       var numberPosition = [];
       for(var i in textoNormal){
-        numberPosition.push(alphabet.indexOf(textoNormal[i].toUpperCase()));
+        numberPosition.push(alphabet.indexOf(textoNormal[i]));
       }
       return numberPosition;
       //console.log(codAscii);
     }
+    //Obter o código ASCII da variável textoNormal;
     var position = getNumberPosition(textoNormal);
     for (var i in position) {
-
-        console.log(position[i] + 65 + 7);
+      return position[i] + 65 + 7);
     }
+  }
+/*
 
-    //Converter o códico ASCII de cada letra à posição dessa mesma letra no alfabeto;
 
     //Aplicar a fórmula (x + n) % 26, onde "x" é a posição da letra e "n = 7" porque é o número de deslocamentos que farei para cada letra;
 
@@ -27,7 +29,7 @@
       for (var charactere = -1; charactere < alphabet.length; i++){
       }
     }
-
+*/
 
 
 
