ce7cbf4fff172f2ff0c489fff653d72fc7663879
Solange Nalão <solangealmn@gmail.com>
2018-06-07 16:21:20 -0300

    Correções no código JavaScript e HTML para implementar a solução com prompt

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
-  //Encriptar texto (cipher)
-  function cipher(){
-    //Obter o valor dos campos;
-    var textoNormal = document.getElementById('cipher-entrance').value.toUpperCase();
-     console.log(textoNormal);
-    //Obter a posição de cada letra;
-    var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    function getNumberPosition(textoNormal){
-      var numberPosition = [];
-      for(var i in textoNormal){
-        numberPosition.push(alphabet.indexOf(textoNormal[i]));
-      }
-      return numberPosition;
-      //console.log(codAscii);
+function cipher(){
+  //Obter o valor dos campos;
+  var texto = prompt("Introduza a frase:").toUpperCase();
+  console.log(texto);
+  //Obter a posição numérica de cada letra;
+  function converte(texto) {
+    var alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var indices = [];
+    for (var i in texto) {
+      indices.push(alfabeto.indexOf(texto[i].toUpperCase()));
     }
-    //Obter o código ASCII da variável textoNormal;
-    var position = getNumberPosition(textoNormal);
-    for (var i in position) {
-      return position[i] + 65 + 7);
-    }
+    return indices;
   }
-/*
-
-
-    //Aplicar a fórmula (x + n) % 26, onde "x" é a posição da letra e "n = 7" porque é o número de deslocamentos que farei para cada letra;
-
-    //validar o valor dos campos;
-    if(textoNormal[i] != ' '){
-      for (var charactere = -1; charactere < alphabet.length; i++){
-      }
-    }
-*/
-
-
-
-    //transformar a variável texto normal em textoEncriptado
-    for(var i = 0; i < textoNormal.length; i++){
-
-    }
-    var textoEncriptado = document.getElementById('cipher-exit').innerHTML;
-    }
-
-  //Decriptar texto (decipher)
-  function decipher(){
-
-    }
+  var position = converte(texto);
+  var codAscii = [];
+  for (var i in position) {
+    codAscii.push(position[i] + 65 + 7 % 26);
+  }
+  var criptTexto = String.charCodeAt(codAscii);
+  return criptTexto;
+}
+cipher();
